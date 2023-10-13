ef9bb1ccffb2ec931afe89387d8b33bc7af13b31
Solkyros <solkyros1@gmail.com>
2023-10-12 22:27:21 -0400

    angular frontend with bootstrap

---
+++ aimagesapp/src/app/side-nav/side-nav.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-nav',
+  templateUrl: './side-nav.component.html',
+  styleUrls: ['./side-nav.component.scss']
+})
+export class SideNavComponent {
+
+sideNavItems = [
+  {
+    id: 0,
+    name: 'Home',
+    icon: 'fa-solid fa-house'
+  },
+  {
+    id: 1,
+    name: 'Upload',
+    icon: 'fa-solid fa-upload'
+  },
+  {
+    id: 2,
+    name: 'Privacy',
+    icon: 'fa-solid fa-lock'
+  },
+  {
+    id: 3,
+    name: 'About',
+    icon: 'fa-solid fa-circle-info'
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    icon: 'fa-solid fa-phone'
+  }
+];
+
+}
